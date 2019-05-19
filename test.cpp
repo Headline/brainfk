e674374d12ccb19a2a04725ff0bc33de2c0f8c29
Michael Flaherty <michaelwflaherty@me.com>
2019-05-18 18:55:51 -0700

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <chrono>
+
+#include "brainfuckvm.h"
+#include "parser.h"
+
+constexpr char code[] = ">++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++[>++++++++<-]>.[-]<<>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++.";
+
+int main(int argc, char *argv[]) noexcept { 
+	Brainfuck::BrainfuckVM vm{std::cout, std::cin};
+
+	auto cb = [](std::string_view str, bool fatal) -> void {
+		printf("\n%s | fatal: %d\n", std::string(str).c_str(), fatal);
+	};
+
+	vm.run(code, cb);
+}
